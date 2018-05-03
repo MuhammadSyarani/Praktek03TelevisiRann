2794e4034ad58ed6d9551face67c59217426ea6f
MuhammadSyarani <32213485+MuhammadSyarani@users.noreply.github.com>
2018-05-03 21:50:20 +0800

    Buat Method CetakInfo()

--- src/praktek03televisirann/Televisi.java
+++ src/praktek03televisirann/Televisi.java
@@ -4,4 +4,10 @@ public class Televisi {
     String merek;
     String warna;
     double harga;
+    
+    void cetakInfo(){
+        System.out.println("Merek \t: "+merek+"\n"+
+                "Warna \t: "+warna+"\n"+
+                "Harga \t:"+harga);
+    }
 }
