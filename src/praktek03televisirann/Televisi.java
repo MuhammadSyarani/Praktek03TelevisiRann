140ec5cff8d586642a78b5a43d6b285e48b04682
MuhammadSyarani <32213485+MuhammadSyarani@users.noreply.github.com>
2018-05-03 21:44:15 +0800

    membuat atribut

--- src/praktek03televisirann/Televisi.java
+++ src/praktek03televisirann/Televisi.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek03televisirann;
 
-/**
- *
- * @author ASUS
- */
 public class Televisi {
-    
+    String merek;
+    String warna;
+    double harga;
 }
