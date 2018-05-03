f5e0400eece3ceb323b991d230bfbc9d8126e909
MuhammadSyarani <32213485+MuhammadSyarani@users.noreply.github.com>
2018-05-03 21:57:43 +0800

    Instance, buat atribut dan panggil method

--- src/praktek03televisirann/TelevisiAksi.java
+++ src/praktek03televisirann/TelevisiAksi.java
@@ -1,7 +1,12 @@
 package praktek03televisirann;
 public class TelevisiAksi {
     public static void main(String[] args) {
+        Televisi t = new Televisi();
+        t.merek="Toshiba";
+        t.warna="Hitam";
+        t.harga=2500;
         
+        t.cetakInfo();
     }
  
 }
