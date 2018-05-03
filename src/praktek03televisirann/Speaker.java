71769484000a6a20785113ae527624150353ee91
MuhammadSyarani <32213485+MuhammadSyarani@users.noreply.github.com>
2018-05-03 22:21:25 +0800

    atribut dan method

--- src/praktek03televisirann/Speaker.java
+++ src/praktek03televisirann/Speaker.java
@@ -1,4 +1,8 @@
 package praktek03televisirann;
 public class Speaker extends Televisi{
+    private String jenisSpeaker;
     
+    void throttle(){
+        System.out.println("DAGUM DAGUM......");
+    }
 }
